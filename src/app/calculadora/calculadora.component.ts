357f4d67f6f9b3ccc9a285952057bff28b5a362d
Denilson <de_nilson_filho@hotmail.com>
2025-07-15 08:59:21 -0300

    conceitos basicos: 15 trabalhando com submuit de formularios

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
 
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  calcularResultado(){
+    this.resultado = this.numero1 + this.numero2;
+    this.numero1 = 0;
+    this.numero2 = 0;
+  }
 }
